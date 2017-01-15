6f2c1851ed0bb76070953cd270f62c21e1d1b0eb
Bob Lail <bob.lailfamily@gmail.com>
2017-01-15 10:46:52 -0600

    [skip] Deleted unused code (1m)

--- app/mailers/houston/releases/mailer.rb
+++ app/mailers/houston/releases/mailer.rb
@@ -43,33 +43,6 @@ module Houston
       end
 
 
-    #   def self.deliver_to!(*recipients)
-    #     recipients.flatten.each do |email|
-    #       developer = ::User.find_by_email!(email)
-    #       Houston.deliver! daily_report(developer)
-    #     end
-    #   end
-    #
-    #   def daily_report(developer, options={})
-    #     @alerts = Houston::Alerts::Alert.open.checked_out_by developer
-    #
-    #     return NullMessage.new if @alerts.none?
-    #
-    #     mail({
-    #       to:       developer,
-    #       subject:  "You have #{@alerts.length} open #{@alerts.length == 1 ? "alert" : "alerts"}",
-    #       template: "houston/alerts/mailer/daily_report"
-    #     })
-    #   end
-    #
-    # private
-    #
-    #   class NullMessage
-    #     def method_missing(*args)
-    #       self
-    #     end
-    #   end
-
     end
   end
 end
